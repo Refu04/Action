7d2ee5adac12d76a11d4718c41efb28213f41738
refu04 <refu28013@gmail.com>
2022-06-06 17:51:55 +0900

    マップの当たり判定を追加

--- Assets/Scripts/Character/PlayerState/Zero/StateBlinking.cs
+++ Assets/Scripts/Character/PlayerState/Zero/StateBlinking.cs
@@ -10,8 +10,24 @@ public class StateBlinking : PlayerStateBase
         //ブリンク開始
         owner.Anim.SetBool("IsBlinking", true);
         owner.Rb.useGravity = false;
+        //タイマーリセット
         time = 0;
-        if (owner.InputEventProvider.MoveDirection.Value.x >= 0)
+        //ブリンク処理
+        //移動値が入力されているか
+        if (owner.InputEventProvider.MoveDirection.Value.x == 0)
+        {
+            //向いている方向にブリンクする
+            if(owner.IsRight)
+            {
+                owner.Rb.velocity = new Vector3(10, 0, 0);
+            } else
+            {
+                owner.Rb.velocity = new Vector3(-10, 0, 0);
+            }
+            
+        }
+        //入力されている方向にブリンクする
+        else if(owner.InputEventProvider.MoveDirection.Value.x > 0)
         {
             owner.Rb.velocity = new Vector3(10, 0, 0);
         } else
